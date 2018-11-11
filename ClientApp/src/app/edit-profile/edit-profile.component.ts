2a2b440abb71474a192509d5ee111c59e235131d
fire_rising <wfhm95@mail.ru>
2018-11-11 16:54:59 +0300

    addon experience fields
    addon experience fields

--- ClientApp/src/app/edit-profile/edit-profile.component.ts
+++ ClientApp/src/app/edit-profile/edit-profile.component.ts
@@ -13,8 +13,11 @@ export class EditProfileComponent implements OnInit {
     bio_text: string = "";
     name_theme_text: string[] = new Array(10);
     requisites_doc_text: string[] = new Array(10);
-    organise_name_text: string = "";
 
+    organise_name_text: string[] = new Array(10);
+    organise_pos_text: string[] = new Array(10);
+    date1_text: string[] = new Array(10);
+    date2_text: string[] = new Array(10);
 
     count_act_subj: number = 0;
     arr_count_act_obj: number[] = Array.from(Array(this.count_act_subj).keys());
@@ -34,6 +37,24 @@ export class EditProfileComponent implements OnInit {
 
     }
 
+
+    count_experience: number = 0;
+    arr_count_experience: number[] = Array.from(Array(this.count_experience).keys());
+
+    incrCountExp() {
+        if (this.count_experience < 10) {
+            this.count_experience += 1;
+            this.arr_count_experience = Array.from(Array(this.count_experience).keys());
+        }
+    }
+
+    decrCountExp() {
+        if (this.count_experience > 0) {
+            this.count_experience -= 1;
+            this.arr_count_experience = Array.from(Array(this.count_experience).keys());
+        }
+
+    }
 
     /*
     flag_act_subj: boolean = true;
